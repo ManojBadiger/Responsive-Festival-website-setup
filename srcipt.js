ace14a1ab779fc357fdf4edc4f0df33e05207f11
ManojBadiger <66901015+ManojBadiger@users.noreply.github.com>
2024-10-26 21:42:34 +0530

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,10 @@
+// GSAP Animation
+
+
+// Toggle Menu Function
+const menu =  document.querySelector(".menu");
+const tolggle = document.getElementById("toggle");
+
+tolggle.onclick = function(){
+    menu.classList.toggle("active")
+}
